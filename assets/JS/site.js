06b96c86470c1294d76044716370d04bc88b775c
mutkovicova <slovakgal@gmail.com>
2022-11-12 15:59:34 +0000

    question 1 validation complete with total count

--- assets/JS/site.js
+++ assets/JS/site.js
@@ -1,16 +1,14 @@
 let total = 0;
 
+/* Question 1 validation */
 const q1Elements = document.querySelectorAll(".q-1-answer");
 
 function handleQ1Selection(event) {
     q1Elements.forEach(element => {
         element.classList.add("disabled");
     })
-    console.log("testing");
     const classes = event.target.classList;
-    console.log(classes);
     const isCorrectAnswer = classes.contains("correct-answer");
-
     if (isCorrectAnswer) {
         event.target.classList.add("btn-outline-success");
         total = total+1;
@@ -18,11 +16,8 @@ function handleQ1Selection(event) {
     else {
         event.target.classList.add("btn-outline-danger");
     } 
+};
 
-}
-
-
-console.log(q1Elements);
 q1Elements.forEach(element => {
     element.addEventListener("click", handleQ1Selection);
 });
